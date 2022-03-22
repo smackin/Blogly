e2d56330020c670b35f921d988b4b6476e99a74d
Stephanie <smmackin12@gmail.com>
2022-03-22 07:43:54 -0400

    added new templates - still getting error

--- app.py
+++ app.py
@@ -1,12 +1,82 @@
-"""Blogly application."""
-
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import database, connect_database, User
+import pdb
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'illnevertell'
 
-connect_db(app)
-db.create_all()
+toolbar = DebugToolbarExtension(app)
+
+connect_database(app)
+database.create_all()
+
+@app.route('/')
+def show_home():
+    """show form to enter into db"""
+    
+    return render_template('base.html')
+
+
+@app.route('/users')
+def users_index():
+    """Show a page with info on all users"""
+
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template('index.html', users=users)
+
+@app.route('/users/new', methods=["GET"])
+def new_users_form():
+    """Show a form to create a new user"""
+    return render_template('new.html')
+
+@app.route("/users/new", methods=["POST"])
+def create_user():
+    """post to create the user and commit to db"""
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    user_img = request.form["image_url"]
+    
+    new_user = User(first_name=first_name, last_name=last_name, image_url=user_img)
+    database.session.add(new_user)
+    database.session.commit()
+    
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>')
+def show_users(user_id):
+    """ show list of all users in DB"""
+    user = User.query.get(user_id)
+    return render_template ('show.html', user=user)
+
+@app.route('/users/<int:user_id>/edit')
+def edit_user(user_id):
+    """use form to edit existing user"""
+    user = User.query.get(user_id)
+    return render_template('edit.html', user=user)
+    
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def update_user(user_id):
+    """submit updated user information. form submission"""
+    user = User.query.get(user_id)
+    user.first_name = request.form["first"]
+    user.last_name = request.form["last"]
+    user.user_img = request.form["image"]
+    
+    database.session.add(user)
+    database.session.commit()
+    
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def delete_user(user_id):
+    
+    user = User.query.get(user_id)
+    database.session.delete(user)
+    database.session.commit()
+    
+    return redirect("/users")
+
+
--- models.py
+++ models.py
@@ -1,1 +1,30 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+database = SQLAlchemy()
+
+DEFAULT_IMG_URL = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fillustrations%2Ficon-silhouettes&psig=AOvVaw2tuGFIkr86jKCzALKTr6gX&ust=1647705564216000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLDIo-CD0PYCFQAAAAAdAAAAABAD"
+
+class User(database.Model):
+    """User info"""
+    
+    __tablename__ = "users"
+    
+    userid = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    first_name = database.Column(database.Text, nullable=False)
+    last_name = database.Column(database.Text, nullable=False)
+    image_url = database.Column(database.Text, nullable=False, default=DEFAULT_IMG_URL) #may add default image if none
+    
+
+#     def greet_user(self):
+#         return f" Hey there {self.first_name} {self.last_name}"
+
+def connect_database(app):
+    """connect database to Flask app"""
+    database.app = app 
+    database.init_app(app)
+    
+    database.app = app 
+    database.init_app(app)
+    
+    
